c5a222df669a966356f0ebb7dcbe620bcb35c001
Lama Aljohani <lmohammedaljohani@stu.kau.edu.sa>
2023-09-12 17:48:06 +0300

    CPIT251-lab2

---
+++ src/main/java/lab2.java
@@ -0,0 +1,12 @@
+
+public class lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi");
+    }
+    
+}

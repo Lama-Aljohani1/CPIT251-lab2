5713b7885ef06e6a66ff72288cb2a06ee3cdb0c9
Ethar-Alomari <144210599+Ethar-Alomari@users.noreply.github.com>
2023-09-13 13:26:51 +0300

    Update lab2.java
    l added a print my name

--- src/main/java/lab2.java
+++ src/main/java/lab2.java
@@ -7,6 +7,7 @@ public class lab2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hi");
+        System.out.println("Ethar Mohammad");
     }
     
 }

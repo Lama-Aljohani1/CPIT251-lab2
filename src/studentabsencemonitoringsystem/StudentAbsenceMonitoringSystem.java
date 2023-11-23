95dcd0296c282f2311c2eb3fedd91e74b0e83d58
Reem-Alhussaini <rhomoodalhussaini@stu.kau.edu.sa>
2023-11-23 17:56:34 +0300

    test

---
+++ src/studentabsencemonitoringsystem/StudentAbsenceMonitoringSystem.java
@@ -0,0 +1,16 @@
+package studentabsencemonitoringsystem;
+
+/**
+ *
+ * @author ryo0m
+ */
+public class StudentAbsenceMonitoringSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
